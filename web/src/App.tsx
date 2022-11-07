849028543623c0cb2b8f438004d2953a068a4793
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-07 19:43:43 -0300

    :arrow_up: Update App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
+import relayEnvironment from './relay/relayEnvironment';
+import Home from './pages/Home';
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Suspense } = React;
 
+export default function App(props: any) {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <RelayEnvironmentProvider environment={relayEnvironment}>
+      <Suspense fallback={'Loading...'}>
+        <Home/>
+      </Suspense>
+    </RelayEnvironmentProvider>
+  );
 }
-
-export default App
