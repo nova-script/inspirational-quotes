e34f1ef045db63fc53889b66e9721137129c4e4f
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-07 19:44:26 -0300

    :bulb: Add app restart and cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,7 @@
 import Koa from 'koa';
 import mount from 'koa-mount';
 import { graphqlHTTP } from 'koa-graphql';
-
+import cors from '@koa/cors';
 import mongoose from 'mongoose';
 
 import GraphQLSchema from './graphql/schema';
@@ -12,14 +12,17 @@ mongoose.connect(
         authSource: "admin",
         user: "root",
         pass: "rootpassword",
+    }, (err) => {
+        if (err) process.exit(1)
     });
 
-mongoose.connection.once('open', () => {
+mongoose.connection.once('open', (err) => {
     console.log('Successfully connected to database!');
 });
 
 const app = new Koa();
 
+app.use(cors());
 app.use(
     mount('/',
         graphqlHTTP({
