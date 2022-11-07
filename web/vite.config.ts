cf7e049f5e2f72a71008239b0b6e4098480e0375
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-07 19:43:57 -0300

    :bulb: Add react-relay plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import relay from "vite-plugin-relay"
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react({
+    babel: {
+      plugins: ['relay']
+    }
+  })]
 })
