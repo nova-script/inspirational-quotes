daef241a8c43e7b6f140919be2aab32e8e4a4274
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-08 15:48:00 -0300

    :bulb: Add Recoil

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { RecoilRoot } from 'recoil';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App/>
+    <RecoilRoot>
+      <App/>
+    </RecoilRoot>
   </React.StrictMode>
 )
