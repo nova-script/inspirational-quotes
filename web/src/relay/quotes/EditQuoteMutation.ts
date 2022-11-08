91332570728cecf59977cf75c3c54e201dccaf7f
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-08 09:55:47 -0300

    :wrench: Fixing mutation

--- web/src/relay/quotes/EditQuoteMutation.ts
+++ web/src/relay/quotes/EditQuoteMutation.ts
@@ -1,11 +1,27 @@
-import { graphql } from 'react-relay';
+import { commitMutation, graphql } from 'react-relay';
 
-export const EditQuoteMutation = graphql`
-  mutation EditQuoteMutation {
-    editQuote(id: $input author: $input quote: $input) {
-        id
-        quote
-        author
+import type { Environment } from 'react-relay';
+
+
+const EditQuoteMutation = graphql`
+  mutation EditQuoteMutation($id: String! $quote: String! $author: String!) {
+    editQuote(id: $id quote: $quote author: $author) {
+      id
+      quote
+      author
     }
   }
 `;
+
+export function commitEditQuoteMutation(environment: any, id: any, quote: any, author: any) {
+  return commitMutation(environment, {
+    mutation: EditQuoteMutation,
+    variables: {
+      id,
+      quote,
+      author
+    },
+    onCompleted: response => {},
+    onError: error => {}
+  })
+}
