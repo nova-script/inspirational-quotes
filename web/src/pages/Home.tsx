11441b87582702fe9b414e01b22553b8fea82556
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-14 15:27:25 -0300

    :arrow_up: Update home preloadedQuery

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,43 +1,21 @@
-import { useState, useEffect } from 'react';
 import {
-  QueryRenderer,
   graphql,
-  useQueryLoader,
-  usePreloadedQuery,
   loadQuery
 } from 'react-relay';
-import { Box, Button, Grid, Typography } from '@mui/material'
-import Container from '@mui/material/Container';
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
-import Quote from '../components/Quote';
-import RelayEnvironment from '../relay/relayEnvironment';
 import { HomeStyle } from '../assets/styles/Home';
-import { AppAllQuotesQuery } from '../relay/quotes/AppAllQuotesQuery';
+import relayEnvironment from '../relay/relayEnvironment';;
+import FetchQuotes from '../components/Quotes/Fetch';
 
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import AddQuote from '../components/AddQuote';
-import { RecoilRoot, atom, useRecoilState } from 'recoil';
-import relayEnvironment from '../relay/relayEnvironment';
-import RenderQuotes from '../components/Quotes/Render';
-import QuotesFetcher from '../components/Quotes/Fetcher';
-
-import type {
-  AppAllQuotesQuery as AppAllQuotesQueryType
-} from '../relay/quotes/__generated__/AppAllQuotesQuery.graphql';
-
-export const refetchAtom = atom({
-  key: 'refetch',
-  default: false,
-});
 
 export const homeAllQuotesQuery = graphql`
-query HomeAllQuotesQuery {
-  queryAllQuotes {
-    id
-    quote
-    author
-  }
-}`;
+  query HomeAllQuotesQuery {
+    queryAllQuotes {
+      id
+      quote
+      author
+    }
+  }`;
 
 const queryReference: any = loadQuery(
   relayEnvironment,
@@ -45,16 +23,12 @@ const queryReference: any = loadQuery(
   {}
 )
 
-
 export default function Home(props: any) {
-  const preloadedQuery = usePreloadedQuery<AppAllQuotesQueryType>(
-    homeAllQuotesQuery, queryReference);
-
   return (
       <div>
         <HomeStyle/>
         <ResponsiveAppBar/>
-        <QuotesFetcher queryReference={preloadedQuery}/>
+        <FetchQuotes queryReference={queryReference}/>
       </div>
   )
 }
