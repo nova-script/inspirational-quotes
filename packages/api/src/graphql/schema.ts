db159c3ee7d9162f7c71949d51e0f87923dabf03
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-15 14:55:03 -0300

    :arrow_up: Adequating modules to Folder-by-Feature structure

--- packages/api/src/graphql/schema.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLSchema } from "graphql";
-
-import Mutations from "./mutations";
-import rootQuery from "./queries/rootQuery";
-
-export default new GraphQLSchema({
-  query: rootQuery,
-  mutation: Mutations,
-});
